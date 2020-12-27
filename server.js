d4521cc171729406539c03c004bc27596898577c
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-12-26 22:20:03 -0600

    add dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 // where your node app starts
 
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
 // init project
 var express = require('express');
 var app = express();
@@ -8,7 +10,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +23,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
